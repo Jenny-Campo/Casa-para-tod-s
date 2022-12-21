138d5250951d7cfa7b51e611b784926041fc551b
AlbertoMG <172amg@gmail.com>
2022-12-21 20:08:12 +0000

    createProvince rute

--- back/api/controllers/province.controller.js
+++ back/api/controllers/province.controller.js
@@ -2,20 +2,20 @@ const Province = require ('../models/province.model')
 const HouseAd = require ('../models/house.model')
 
 
-// async function createProvince (req, res) {
-//     try {
-//         const listProvince = await Province.bulkCreate([
-//             {name: 'Alava'},{name: 'Albacete'},{name: 'Alicante'},{name: 'Almería'},{name: 'Asturias'},{name: 'Avila'},{name: 'Badajoz'},{name: 'Barcelona'},{name: 'Burgos'},{name: 'Cáceres'},
-//             {name: 'Cádiz'},{name: 'Cantabria'},{name: 'Castellón'},{name: 'Ciudad Real'},{name: 'Córdoba'},{name: 'La Coruña'},{name: 'Cuenca'},{name: 'Gerona'},{name: 'Granada'},{name: 'Guadalajara'},
-//             {name: 'Guipúzcoa'},{name: 'Huelva'},{name: 'Huesca'},{name: 'Islas Baleares'},{name: 'Jaén'},{name: 'León'},{name: 'Lérida'},{name: 'Lugo'},{name: 'Madrid'},{name: 'Málaga'},{name: 'Murcia'},{name: 'Navarra'},
-//             {name: 'Orense'},{name: 'Palencia'},{name: 'Las Palmas'},{name: 'Pontevedra'},{name: 'La Rioja'},{name: 'Salamanca'},{name: 'Segovia'},{name: 'Sevilla'},{name: 'Soria'},{name: 'Tarragona'},
-//             {name: 'Santa Cruz de Tenerife'},{name: 'Teruel'},{name: 'Toledo'},{name: 'Valencia'},{name: 'Valladolid'},{name: 'Vizcaya'},{name: 'Zamora'},{name: 'Zaragoza'}
-//         ])
+async function createProvince (req, res) {
+    try {
+        const listProvince = await Province.bulkCreate([
+            {name: 'Alava'},{name: 'Albacete'},{name: 'Alicante'},{name: 'Almería'},{name: 'Asturias'},{name: 'Avila'},{name: 'Badajoz'},{name: 'Barcelona'},{name: 'Burgos'},{name: 'Cáceres'},
+            {name: 'Cádiz'},{name: 'Cantabria'},{name: 'Castellón'},{name: 'Ciudad Real'},{name: 'Córdoba'},{name: 'La Coruña'},{name: 'Cuenca'},{name: 'Gerona'},{name: 'Granada'},{name: 'Guadalajara'},
+            {name: 'Guipúzcoa'},{name: 'Huelva'},{name: 'Huesca'},{name: 'Islas Baleares'},{name: 'Jaén'},{name: 'León'},{name: 'Lérida'},{name: 'Lugo'},{name: 'Madrid'},{name: 'Málaga'},{name: 'Murcia'},{name: 'Navarra'},
+            {name: 'Orense'},{name: 'Palencia'},{name: 'Las Palmas'},{name: 'Pontevedra'},{name: 'La Rioja'},{name: 'Salamanca'},{name: 'Segovia'},{name: 'Sevilla'},{name: 'Soria'},{name: 'Tarragona'},
+            {name: 'Santa Cruz de Tenerife'},{name: 'Teruel'},{name: 'Toledo'},{name: 'Valencia'},{name: 'Valladolid'},{name: 'Vizcaya'},{name: 'Zamora'},{name: 'Zaragoza'}
+        ])
         
-//     } catch (error) {
-//         return res.status(500).send(error.message)
-//     }
-// }
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
 
 async function getAllProvince (req, res) {
     try {
@@ -42,7 +42,7 @@ async function findProvince (req, res) {
 }
 
 module.exports = {
-    //createProvince,
+    createProvince,
     getAllProvince,
     findProvince
 }
--- back/api/routes/province.router.js
+++ back/api/routes/province.router.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
-const { checkAuth } = require('../utils')
 
 const {
     getAllProvince,
     findProvince,
+    createProvince
 } = require('../controllers/province.controller')
 
 router.get('/', getAllProvince)
 router.get('/:province', findProvince)
+router.post('/province', createProvince )
 
 module.exports = router
