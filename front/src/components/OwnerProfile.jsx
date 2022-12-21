4b4c766f8cb44b47d6f3139518b6ae937cebcb70
AlbertoMG <172amg@gmail.com>
2022-12-21 19:42:51 +0000

    ownercard donde , adcard done

--- front/src/components/OwnerProfile.jsx
+++ front/src/components/OwnerProfile.jsx
@@ -10,7 +10,7 @@ const CARD = {
     display: 'flex',
     borderRadius: 10,
     marginTop: '20px',
-    color:'dark',
+    color:'#172B4D',
     justifyContent:'space-between'
   }
 
@@ -55,35 +55,38 @@ function OwnerProfile() {
         </Grid>
                 <Grid xs={6}>
                 <CardContent>
+                <Typography sx={{textDecoration: 'underline', color:'dark'}} variant="h3" component="div">
+                    Perfil del propietari@
+                </Typography> 
                 <Typography variant="h6" component="div">
-                    { user.name } { user.surname }
+                   <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Nombre y Apellidos:</Typography> { user.name } { user.surname }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.aboutYou }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Sobre mí:</Typography> { user.aboutYou }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.lookingFor }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Busco:</Typography> { user.lookingFor }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.hobbies }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Aficiones:</Typography> { user.hobbies }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.smoker }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Fumador:</Typography> { user.smoker }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.pets }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Mascotas:</Typography> { user.pets }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.emplyment }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Estado laboral:</Typography> { user.employment }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.rules }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Normas:</Typography> { user.rules }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.roommate }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Me gustaría compartir con:</Typography> { user.roommate }
                 </Typography>
                 <Typography variant="h6" component="div">
-                    { user.phoneNumber }
+                    <Typography variant="h6" component="div" display="inline" sx={{color: 'dark', fontStyle: 'italic'}}>- Teléfono:</Typography> { user.phoneNumber }
                 </Typography>
                 </CardContent>
                 </Grid>
